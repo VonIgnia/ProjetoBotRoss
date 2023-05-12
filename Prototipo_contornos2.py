4bd227a91735555167d5b67238b4a0c7000b0a75
VonIgnia <giovanniar@al.insper.edu.br>
2023-05-12 09:34:25 -0300

    Slicing_contornos feito

--- Prototipo_contornos2.py
+++ Prototipo_contornos2.py
@@ -1,26 +1,29 @@
-lista = [[120,60,10],[60,120,0],[0,120,0],[0,0,0],[60,120,60]]
+lista = [[120,60,10],[60,120,0],[0,120,0],[0,0,0],[60,120,60]] #pontos em formato [[x1,y1,z1],[x2,y2,z2]...]
+T = len(lista) 
 
-X = []
-Y = []
-Z = []
-
-T = len(lista) #pontos em formato [[x1,y1,z1],[x2,y2,z2]...]
-tam_max_comm = 2 #numero de pontos que serão passados simultaneamente para o robo
+tam_max_comm = 2 #numero de pontos que serão passados a cada vez para o robo
+cpi = 0 #indica o indice do ponto atual na lista T(current point index)
 
 #enquanto o tamanho da lista que vai ser comunicada for menor do que o tamanho comunicável
-    #percorrer cada elemento da lista e adicionar às listas que serão comunicadas
-itens_enviados = 0
-while itens_enviados <= T:
-    for i in lista:
-        X.append(i[0])
-        Y.append(i[1])
-        Z.append(i[2])
-        if len(Z) == tam_max_comm:
-            break
+#percorrer cada elemento da lista e adicionar às listas que serão comunicadas
+
+while cpi < T:
+    #se o numero de pontos da lista que faltam ser comunicados forem menores do que o valor de pontos que serão comunicados
+    if T - cpi < tam_max_comm:
+        tam_max_comm = T-cpi #numero de pontos que serão passados a cada vez para o robo
+
+    X = []
+    Y = []
+    Z = []
+    while len(Z) < tam_max_comm:
+        X.append(lista[cpi][0])
+        Y.append(lista[cpi][1])
+        Z.append(lista[cpi][2])
+        cpi+=1
+
     print ("X", X)
     print ("Y", Y)
     print ("Z", Z)
-    itens_enviados += 1 
 
 
 

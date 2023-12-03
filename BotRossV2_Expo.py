7ca4cdc1ac062f0712705f82e5c31c4db4226d8f
Giovanni <giovanniar@al.insper.edu.br>
2023-12-03 19:10:54 -0300

    Split_Colors working as it should, new images.

--- BotRossV2_Expo.py
+++ BotRossV2_Expo.py
@@ -24,37 +24,96 @@ PORT_RECEIVE = 12346 # Replace with the desired port for receiving data from the
 
 ### Code
 
-# Function to handle receiving data from UR5
-def receive_data_from_ur5():
-    receive_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    receive_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-    receive_socket.bind((HOST, PORT_RECEIVE))
-    receive_socket.listen(5)
-    c, addr = s.accept()
-    if (addr[0] != ''):
-        reciever_connected = True
-        print(f"Listening for UR5 data on {HOST}:{PORT_RECEIVE}")
-    return reciever_connected
+import cv2
+import matplotlib.pyplot as plt
+
+import numpy as np
+from math import *
+import FunctionsV2
+
+import socket
+import time
+import threading
 
 
-# Function to handle sending instructions to UR5
-def send_instructions_to_ur5():
-    send_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    send_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-    send_socket.bind((HOST, PORT_RECEIVE))
-    send_socket.listen(5)
-    c, addr = s.accept()
-    if (addr[0] != ''):
-        sender_connected = True
-        print(f"Connected to UR5 for sending instructions on {HOST}:{PORT_SEND}")
-    return sender_connected
-
-img_in = cv2.imread("imgs_avancadas/birb.jpeg", cv2.IMREAD_COLOR)
+img_in = cv2.imread(FunctionsV2.select_image(), cv2.IMREAD_COLOR)
+#img_in = cv2.imread("imgs_avancadas\PatBenatar.jpg", cv2.IMREAD_COLOR)
 if img_in is None:
     print("File not found. Bye!")
     exit(0)
-    
 
+A4_retrato = (297,210) #dimensões em mm (height, width)
+A4_paisagem = (210,297) #dimensões em mm (height, width)
+
+resized_image = FunctionsV2.resize_keeping_aspect_ratio(img_in,A4_retrato)
+
+"""
+cv2.imshow('Resized Image', resized_image)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
+"""
+
+#imagem que o robô irá desenhar:
+#img_v = cv2.flip(hsv_img_in_norm, 0) # flip the image by vertically
+#img_h = cv2.flip(hsv_img_in_norm, 1) # flip the image by horizontally
+#img_in_norm_vh = cv2.flip(hsv_resized_image,-1) # flip the image in both axis
+
+#cores + preto e branco
+listaHSV_Cores_Canetas =  [[0,0,100],[0,100,80],[24,100,100],
+                           [60,100,100],[137,85.5,43.1],[197,70,83.5],
+                           [214, 94.3, 64.7],[326, 71.9, 57.3],[240,80,25],
+                           [0,0,10]]
+
+#lista reduzida para testes (cores quentes)
+#listaHSV_Cores_Canetas =  [[0,0,100],[0,100,80],[24,100,100],[60,100,100]]
+
+#lista reduzida para testes (cores frias)
+#listaHSV_Cores_Canetas =  [[0,0,100],[197,70,83.5],[214, 94.3, 64.7],[326, 71.9, 57.3],[240,80,25]]
+
+simplified_colors = FunctionsV2.Simplify_Image_Colors(resized_image,listaHSV_Cores_Canetas)
+simplified_colors_RGB = cv2.cvtColor(simplified_colors, cv2.COLOR_HSV2BGR)
+
+# Display the image with simplified colours
+cv2.imshow('cores_finais', simplified_colors_RGB)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
+
+dict_filings_by_color = FunctionsV2.Split_Colors(simplified_colors, listaHSV_Cores_Canetas)
+#print(dict_filings_by_color.keys())
+dict_point_positions_by_color = {}
+for element in dict_filings_by_color.keys():
+    RGB_split_preview = cv2.cvtColor(dict_filings_by_color[element], cv2.COLOR_HSV2BGR)
+    cv2.imshow(element, RGB_split_preview)
+    cv2.waitKey(0)
+    
+    binirized_color =  cv2.cvtColor(dict_filings_by_color[element], cv2.COLOR_BGR2GRAY)
+    returns, thresh = cv2.threshold(binirized_color, 1, 255, cv2.THRESH_BINARY)
+    point_positions = FunctionsV2.Generate_fillings(thresh, element)
+"""
+(height,width) = (simplified_colors.shape[0], simplified_colors.shape[1])
+
+#creates a blank one channel image into a blank three channel image in order to pass HSV or RGB values to it
+img_out = np.zeros((height,width), dtype="uint8")
+img_out = cv2.cvtColor(img_out,cv2.COLOR_GRAY2BGR)
+
+for color in listaHSV_Cores_Canetas:
+    for i in range(height-1):
+        for j in range(width-1):
+            if np.all(simplified_colors[i,j] == color):
+                img_out[i,j] =  color
+            else:
+                img_out[i,j] = [0,0,0]
+
+    img_out = cv2.cvtColor(img_out, cv2.COLOR_HSV2BGR)
+    grayscale_colors =  cv2.cvtColor(img_out, cv2.COLOR_BGR2GRAY)
+    returns, thresh = cv2.threshold(grayscale_colors, 1, 255, cv2.THRESH_BINARY)
+
+    cv2.imshow(str(color), img_out)
+    cv2.waitKey(0)
+
+    point_positions = FunctionsV2.Generate_fillings(thresh)
+    #cv2.destroyAllWindows()
+"""
 
 print ('Program Started')
 print('Trying Connection')
--- Projeto_V5_papagaio.py
+++ Projeto_V5_papagaio.py
@@ -5,6 +5,11 @@ import numpy as np
 from math import *
 import FunctionsV2
 
+import socket
+import time
+import threading
+
+
 img_in = cv2.imread(FunctionsV2.select_image(), cv2.IMREAD_COLOR)
 if img_in is None:
     print("File not found. Bye!")
@@ -33,10 +38,10 @@ cv2.destroyAllWindows()
 #sem preto, nem branco
 #listaHSV_Cores_Canetas =  [[0,100,80],[24,100,100],[60,100,100],[137,85.5,43.1],[197,70,83.5],[214, 94.3, 48.2],[326, 71.9, 57.3],[0,0,0]]
 #cores + preto e branco
-#listaHSV_Cores_Canetas =  [[0,0,100],[0,100,80],[24,100,100],[60,100,100],[137,85.5,43.1],[197,70,83.5],[214, 94.3, 64.7],[326, 71.9, 57.3],[240,80,25]]
+listaHSV_Cores_Canetas =  [[0,0,100],[0,100,80],[24,100,100],[60,100,100],[137,85.5,43.1],[197,70,83.5],[214, 94.3, 64.7],[326, 71.9, 57.3],[240,80,25],[0,0,0]]
 
 #lista reduzida para testes
-listaHSV_Cores_Canetas =  [[0,0,100],[0,100,80],[24,100,100],[60,100,100]]
+#listaHSV_Cores_Canetas =  [[0,0,100],[0,100,80],[24,100,100],[60,100,100]]
 
 hsv_img_out = FunctionsV2.Simplify_Image_Colors(resized_image,listaHSV_Cores_Canetas)
 simplified_colors = cv2.cvtColor(hsv_img_out, cv2.COLOR_HSV2BGR)
@@ -61,21 +66,98 @@ for color in listaHSV_Cores_Canetas:
     grayscale_colors =  cv2.cvtColor(img_out, cv2.COLOR_BGR2GRAY)
     returns, thresh = cv2.threshold(grayscale_colors, 1, 255, cv2.THRESH_BINARY)
 
-    #cv2.imshow(str(color), img_out)
-    #cv2.waitKey(0)
-
-    FunctionsV2.Gera_preenchimento_Vf(thresh)
-    #cv2.imshow(str(color), thresh)
+    cv2.imshow(str(color), img_out)
     cv2.waitKey(0)
+
+    point_positions = FunctionsV2.Generate_fillings(thresh)
+    #cv2.imshow(str(color), thresh)
+    #cv2.waitKey(0)
     
+    print ('UR5 Communication')
+
+    #HOST = '10.103.16.140'
+    HOST = '10.103.16.11'
+    PORT = 20000
+
+    print('Trying Connection')
+    count = 0
+
+    connected = False
+    while (connected == False):
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        s.bind((HOST, PORT))
+        s.listen(5)
+        c, addr = s.accept()
+        if (addr[0] != ''):
+            connected = True
+            print("Connected")
+            
+    lista=point_positions 
+    lista.insert(0, list(np.add(lista[0],[0,0,60]))) #acrescenta movimento em Z no início do contorno para não rabiscar entre contornos
+    lista.append(list(np.add(lista[-1],[0,0,60])))   #acrescenta movimento em Z no fim do contorno para não rabiscar entre contornos
+
+    T = len(lista)
+
+    tam_max_comm = 10 #numero de pontos que serão passados a cada vez para o robo
+
+    n_appends = tam_max_comm - ( T% tam_max_comm)
+    print (n_appends)
+    while n_appends != 0:
+        lista.append([0,0,0])
+        n_appends = n_appends-1
+
+    T = len(lista)
+
+    cpi = 0 #indica o indice do ponto atual na lista T(current point index)
+
+    #enquanto o tamanho da lista que vai ser comunicada for menor do que o tamanho comunicável
+    #percorrer cada elemento da lista e adicionar às listas que serão comunicadas
+    pontos_enviados = 0
+
+    print(lista)
+
+    while pontos_enviados < T: #enquanto o indice do ponto atual for mentor que o compriomento total da lista de pontos
+        if T - cpi < tam_max_comm: #se o numero de pontos da lista que faltam ser comunicados forem menores do que o valor de pontos que serão comunicados
+            tam_max_comm = T-cpi   #numero de pontos que serão passados a cada vez para o robo
+
+        X = [] #X ,Y e Z irão conter t_max_comm coordenadas
+        Y = []
+        Z = []
+        while len(Z) < tam_max_comm:
+            X.append(lista[cpi][0])
+            Y.append(lista[cpi][1])
+            Z.append(lista[cpi][2])
+            cpi+=1
+
+        try:
+            c.send(str([tam_max_comm]).encode('ascii')) #tamanho das listas X, Y e Z
+            print ("num_pontos", [tam_max_comm])
+            time.sleep(0)
+            c.send(str(X).encode('ascii')) #codifica (x,y,z,Rx,Ry,Rz) para um formato compreendido pelo robô
+            print ("X", X)
+            time.sleep(0)
+            c.send(str(Y).encode('ascii')) #codifica (x,y,z,Rx,Ry,Rz) para um formato compreendido pelo robô
+            print ("Y", Y)
+            time.sleep(0)
+            c.send(str(Z).encode('ascii')) #codifica (x,y,z,Rx,Ry,Rz) para um formato compreendido pelo robô
+            print ("Z", Z)
+            time.sleep(0)
+        
+        except socket.error as socketerror:
+            print (count)
+
+        pontos_enviados+=tam_max_comm
+        count += 1
+
+
+    c.close()
+    s.close()
+    print('Disconnected')
+    print('Finished Colour')
 
 #dictionary contining one binary image for each available color
 #impossible to store image on dict (unhashable type: "list")
-dict_splited_colors = FunctionsV2.Split_Colors(simplified_colors,listaHSV_Cores_Canetas) 
-
-# Display the image with contours
-#cv2.imshow('cores_finais', simplified_colors)
-#cv2.waitKey(0)
-#cv2.destroyAllWindows()    
+#dict_splited_colors = FunctionsV2.Split_Colors(simplified_colors,listaHSV_Cores_Canetas) 
 
 cv2.destroyAllWindows()
